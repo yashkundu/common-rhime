cf71464858737320dd332010b292cd627ddeb951
yashkundu <yashasvik107@gmail.com>
2022-10-13 00:01:00 +0530

    Updates

---
+++ src/middlewares/queryObjectIdValidator.ts
@@ -0,0 +1,14 @@
+import {Request, Response, NextFunction} from 'express'
+import { objectIdValidator } from '../utils/objectIdValidator'
+import { BadRequestError } from '../errors/badRequestError'
+
+// this is a generator for a middleware to verify object Id of a query
+
+export const paramObjectIdValidator = (field: string) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        const queryId = req.query[field] as string
+        if(!queryId || !objectIdValidator(queryId)) 
+            throw new BadRequestError(`Invalid ${field} in the url param`)
+        next()
+    }
+}
